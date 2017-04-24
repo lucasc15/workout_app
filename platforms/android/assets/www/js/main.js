e5fe344168b1f199cf5b6e94b59fad4f660950b6
lucas <currahl@yahoo.ca>
2017-04-24 14:08:16 -0400

    Added platforms to .gitignore

--- platforms/android/assets/www/js/main.js
+++
@@ -1,9 +0,0 @@
- 
-var initialize = function() {
-  oStatus = document.getElementById("test");
-  oStatus.innerHTML = "Connected!";
-  oStatus.style.color = "green";
-  console.log("Got here!");
-}
-
-document.addEventListener("deviceready", initialize, false);
